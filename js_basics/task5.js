a2cb49a61c7402811f5d7434c77f97cb298fc084
olenanikitina1321 <o.nikitina@geniusee.com>
2023-10-16 16:25:45 +0200

    homework#2

---
+++ js_basics/task5.js
@@ -0,0 +1,21 @@
+// 5.1
+const radius = 5
+const π = Math.PI
+
+const circleArea = π * Math.pow(radius, 2)
+console.log(+circleArea.toFixed(2))
+
+// 5.2
+const length = 10.4
+const width = 13.21
+
+const rectangleArea = length * width
+console.log(+rectangleArea.toFixed(2))
+
+// 5.3
+const height = 11
+const cylinderRadius = 10
+
+const cylinderVolume = π * Math.pow(cylinderRadius, 2) * height
+console.log(+cylinderVolume.toFixed(2))
+
