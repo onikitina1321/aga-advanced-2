2d6f9454f793b6105ab1fb770ff4d80eff13e9ca
olenanikitina1321 <o.nikitina@geniusee.com>
2023-10-26 22:14:28 +0200

    Homework_arrays

---
+++ arrays/task2.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4, 5]
+
+const newNumbers = numbers.map((number, index) => number * index)
+console.log(newNumbers)
---
+++ arrays/task5.js
@@ -0,0 +1,6 @@
+
+const firstArray = [1, 24, 31, 'Olena']
+const secondArray = [13, 21, 11]
+
+const thirdArray = firstArray.concat(secondArray)
+console.log(thirdArray)
