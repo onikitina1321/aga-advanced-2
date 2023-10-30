effc3f39f022d74921fe9f576b32ecedc438a57b
olenanikitina1321 <o.nikitina@geniusee.com>
2023-10-30 22:53:16 +0100

    Homework objects

---
+++ objects/task3.js
@@ -0,0 +1,8 @@
+const car = {
+    brand: 'Mercedes',
+    model: 'GLE',
+    year: '2022'
+}
+
+car.year = 2023
+console.log(car)
---
+++ objects/task5.js
@@ -0,0 +1,18 @@
+const user = [
+    {
+        name: 'Olena',
+        age: 31,
+        email: 'adhellam@gmail.com'},
+    {
+        name: 'Olha',
+        age: 11,
+        email: 'olha@gmail.com'},
+    {
+        name: 'Max',
+        age: 18,
+        email: 'max@gmail.com'}
+]
+
+for (const {name , age, email} of user) {
+    console.log(`${name} is ${age} years old and has email address ${email}`)
+}
